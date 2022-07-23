8933150bb85dda099b55faa4b9b46b6ad97055df
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-07-23 19:51:00 +0300

    create Utils

--- src/main/java/com/example/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/example/weatherapp/WeatherAppApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.weatherapp.entity.Inputdata;
+import com.example.weatherapp.service.Utils;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
@@ -19,17 +20,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 public class WeatherAppApplication {
 
-	public static void main(String[] args) throws StreamReadException, DatabindException, IOException {
+	public static void main(String[] args)  {
 		SpringApplication.run(WeatherAppApplication.class, args);
 		
-		final String JSON_DATA = "/citys.json";
-		
-		TypeReference<List<Inputdata>> typeReference = new TypeReference<List<Inputdata>>() {
-		};
-		
-		InputStream inputStream = TypeReference.class.getResourceAsStream(JSON_DATA);
-		List <Inputdata> data = new ObjectMapper().readValue(inputStream, typeReference);
-		System.out.println(data);
+		Utils.getDataFromJsonFile();
 		
 		
 	}
---
+++ src/main/java/com/example/weatherapp/service/Utils.java
@@ -0,0 +1,45 @@
+package com.example.weatherapp.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import com.example.weatherapp.entity.Inputdata;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utils {
+	
+	public static void name() {
+		System.out.println("hello");
+	}
+	
+	public static List<Inputdata> getDataFromJsonFile() {
+		
+		final String JSON_DATA = "/citys.json";
+		
+		TypeReference<List<Inputdata>> typeReference = new TypeReference<List<Inputdata>>() {
+		};
+		
+		InputStream inputStream = TypeReference.class.getResourceAsStream(JSON_DATA);
+		List<Inputdata> data = null;
+		try {
+			data = new ObjectMapper().readValue(inputStream, typeReference);
+		} catch (StreamReadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DatabindException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(data);
+		return data;
+		
+	}
+
+}
