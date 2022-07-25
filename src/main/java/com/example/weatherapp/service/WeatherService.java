aee219f17003d9e2348d98fefaa2c9e04614957c
Yoram Nagavker <48060702+yoramnag@users.noreply.github.com>
2022-07-25 12:08:12 +0300

    adding Thread Pool

--- src/main/java/com/example/weatherapp/service/WeatherService.java
+++ src/main/java/com/example/weatherapp/service/WeatherService.java
@@ -1,6 +1,11 @@
 package com.example.weatherapp.service;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +17,8 @@ public class WeatherService {
 	@Value("${spring.datasource.appid}")
 	String appid;
 	
+	Logger logger = LoggerFactory.getLogger(WeatherService.class);
+	
 	public WeatherResponse returnWeatherForACity(String cityName) {
 		String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName 
 				+ "&"+ "appid=" + appid +"&units=metric";
@@ -19,4 +26,15 @@ public class WeatherService {
 		WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
 		return weatherResponse;
 	}
+	
+//	//@Async
+//	public CompletableFuture<WeatherResponse> returnWeatherForACity(String cityName) {
+//		logger.info("get WeatherResponse by " + Thread.currentThread().getName());
+////		String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName 
+////				+ "&"+ "appid=" + appid +"&units=metric";
+//		String url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e735b6b632e6c008be941b8dbdb346d4&units=metric";
+//		RestTemplate restTemplate = new RestTemplate();
+//		WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
+//		return CompletableFuture.completedFuture(weatherResponse);
+//	}
 }
